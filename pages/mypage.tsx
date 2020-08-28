51363fda8c1bacf05d0e32b25d20212be54c7ea0
hironakazuki <far_east_nightbird30@icloud.com>
2020-08-28 16:15:30 +0900

    setting titleTag in a HeadTag

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,48 +1,37 @@
 import React from "react";
-import { API, graphqlOperation } from "aws-amplify";
-import { GetServerSideProps } from "next";
-import {
-  AuthTokens,
-  useAuth,
-  useAuthFunctions,
-  getServerSideAuth,
-} from "../auth";
+import Head from "next/head";
 
-import { createRoom } from "../src/graphql/mutations";
-import { onCreateRoom } from "../src/graphql/subscriptions";
-import { listRooms } from "../src/graphql/queries";
+import { useAuth } from "../auth";
 
-import { ListRoomsQuery, OnCreateRoomSubscription } from "../src/API";
-import {
-  useStateValue,
-  setRoomsList,
-  createRoomSubscription,
-} from "../src/state";
-useStateValue;
 import GenericTemplate from "../components/templates/GenericTemplate";
 
 const MyPage = () => {
   const auth = useAuth(null);
   return (
-    <GenericTemplate title="マイページ">
-      {auth ? (
-        <React.Fragment>
-          <h4>IdTokenData</h4>
-          <div>
-            <React.Fragment>
-              <p>{auth.idTokenData.email}</p>
-              <p>{auth.idTokenData["cognito:username"]}</p>
-            </React.Fragment>
-          </div>
-          <h4>AccessTokenData</h4>
-          <div>{auth.accessTokenData.username}</div>
-        </React.Fragment>
-      ) : (
-        <p>
-          <small>ログインしてください。</small>
-        </p>
-      )}
-    </GenericTemplate>
+    <>
+      <Head>
+        <title>{auth.accessTokenData.username} | next-chat-app</title>
+      </Head>
+      <GenericTemplate title="マイページ">
+        {auth ? (
+          <React.Fragment>
+            <h4>IdTokenData</h4>
+            <div>
+              <React.Fragment>
+                <p>{auth.idTokenData.email}</p>
+                <p>{auth.idTokenData["cognito:username"]}</p>
+              </React.Fragment>
+            </div>
+            <h4>AccessTokenData</h4>
+            <div>{auth.accessTokenData.username}</div>
+          </React.Fragment>
+        ) : (
+          <p>
+            <small>ログインしてください。</small>
+          </p>
+        )}
+      </GenericTemplate>
+    </>
   );
 };
 
